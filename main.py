8699a8c574e35bbc74eefd37756516eab98a4d6f
SHIZA-gaming <65359498+SHIZA-gaming@users.noreply.github.com>
2021-11-17 16:55:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
 
-menuApp = ["Установка", "Первое приложение", "Обратная связь"]
+app.config['SECRET_KEY'] = 'jknlkmvlfndfjvbdhvhggv'
+
+menuApp = [{'name': 'Установка', 'url': 'install-flask'},
+           {'name': 'Первое приложение', 'url': 'first-app'},
+           {'name': 'Обратная связь', 'url': 'contact'}]
 
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menuApp)
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте",  menu=menuApp)
+
+@app.route("/profile/<path:username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        if len(request.form["username"]) > 2:
+            flash('Сообщение отправлено', category='success')
+        elif len(request.form["email"]) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+    return render_template('contact.html', title='Обратная связь', menu=menuApp)
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username="selfedu"))
+
 
 if __name__=="__main__":
     app.run(debug=True)
